1be6a05e72e6738445691f7a6c41bd19d6e3af32
Hanan Qaisar (Linux) <hananqaisar316@gmail.com>
2026-06-10 16:01:26 +0500

    clean

--- nova_unlock/__init__.py
+++
@@ -1,7 +0,0 @@
-
-# V5.2 new features
-try:
-    from nova_unlock.vision.liveness    import LivenessDetector
-    from nova_unlock.ui.theme_manager   import ThemeManager, get_theme
-except ImportError:
-    pass
